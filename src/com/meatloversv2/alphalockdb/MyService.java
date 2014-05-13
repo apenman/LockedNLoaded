a7f89c03fb7a2e40c489a69f7aa58a0febd9c301
Alex Penman <apenman427@gmail.com>
2014-05-13 11:07:42 -0700

    Update MyService.java

--- src/com/meatloversv2/alphalockdb/MyService.java
+++ src/com/meatloversv2/alphalockdb/MyService.java
@@ -46,17 +46,6 @@ public void onCreate() {
      k1= km.newKeyguardLock("IN");
      k1.disableKeyguard();
 
-
-
-
-     /*try{
-     StateListener phoneStateListener = new StateListener();
-     TelephonyManager telephonyManager =(TelephonyManager)getSystemService(TELEPHONY_SERVICE);
-     telephonyManager.listen(phoneStateListener,PhoneStateListener.LISTEN_CALL_STATE);
-     }catch(Exception e){
-    	 System.out.println(e);
-     }*/
-    
 		IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_ON);
 		filter.addAction(Intent.ACTION_SCREEN_OFF);
 
@@ -72,46 +61,6 @@ public void onStart(Intent intent, int startId) {
 
 	super.onStart(intent, startId);
 }
-
-/*class StateListener extends PhoneStateListener{
-    @Override
-    public void onCallStateChanged(int state, String incomingNumber) {
-
-        super.onCallStateChanged(state, incomingNumber);
-        switch(state){
-            case TelephonyManager.CALL_STATE_RINGING:
-                break;
-            case TelephonyManager.CALL_STATE_OFFHOOK:
-                System.out.println("call Activity off hook");
-            	getApplication().startActivity(myIntent);
-
-
-
-                break;
-            case TelephonyManager.CALL_STATE_IDLE:
-                break;
-        }
-    }
-};*/
-
-
-/*protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-	Log.d("res", "checking");
- if(requestCode == REQ_ENTER_PATTERN) {
-		if(resultCode == 1) {
-			String packName = data.getStringExtra("packageName");
-			Log.d("okay", "starting : " + packName);
-		    PackageManager  pmi = getPackageManager();
-		    Intent intent = null;
-
-		    intent = pmi.getLaunchIntentForPackage(packName);                    
-		    if (intent != null){
-		    	startActivity(intent);
-		    }
-		}
-		// START ACTIVITY WITH PACKAGE NAME FROM EXTRA
-	}
-}*/
 
 @Override
 public void onDestroy() {
